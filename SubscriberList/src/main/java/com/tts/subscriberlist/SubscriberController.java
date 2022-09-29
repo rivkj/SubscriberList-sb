18331c679986a6890a16fd74009983d163c0a371
kimberlyrivas <rivaskj07@gmail.com>
2022-09-29 18:50:33 -0400

    first commit

---
+++ SubscriberList/src/main/java/com/tts/subscriberlist/SubscriberController.java
@@ -0,0 +1,31 @@
+package com.tts.subscriberlist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SubscriberController {
+	
+	@Autowired
+	  private SubscriberRepository subscriberRepository;
+	
+	@GetMapping
+	public String index(Subscriber subscriber) {
+		return "subscriber/index";
+	    }
+	private Subscriber subscriber;
+	
+	@PostMapping(value = "/")
+	public String addNewSubscriber(Subscriber subscriber, Model model) {
+		subscriberRepository.save(new Subscriber(subscriber.getFirstName(), 
+	        subscriber.getLastName(), subscriber.getUserName(), subscriber.getSignedUp()));
+		model.addAttribute("firstName", subscriber.getFirstName());
+		model.addAttribute("lastName", subscriber.getLastName());
+		model.addAttribute("userName", subscriber.getUserName());
+		return "subscriber/result";
+	}
+}
